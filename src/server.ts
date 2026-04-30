d928047997e0e2266ab5611ec05ee9200c0e80b6
ashiful islam <ashifulislam2002@gmail.com>
2026-04-30 22:51:58 +0600

    added handller in server.ts file

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,14 @@
+import { Server } from "http";
 import app from "./app";
 import { envVars } from "./app/config/env";
 import { seedSuperAdmin } from "./app/utils/seed";
 
+
+let server: Server
 async function main() {
   try {
-    // await seedSuperAdmin();
-    app.listen(envVars.PORT, () => {
+    await seedSuperAdmin();
+    server = app.listen(envVars.PORT, () => {
       console.log(
         `Example app listening on port http://localhost:${envVars.PORT}`
       );
@@ -15,9 +18,57 @@ async function main() {
   }
 }
 
+// SIGTERM signal error
+process.on("SIGTERM", () => {
+  console.log("SIGTERM signal detected. Closing server...");
+
+  if (server) {
+    server.close(() => {
+      console.log("Server closed");
+      process.exit(1);
+    });
+  }
+  process.exit(1);
+});
+
+// SIGINT signal error
+process.on("SIGINT", () => {
+  console.log("SIGINT signal detected. Closing server...");
+
+  if (server) {
+    server.close(() => {
+      console.log("Server closed");
+      process.exit(1);
+    });
+  }
+  process.exit(1);
+});
+
+
+//uncaught exception
+process.on("uncaughtException", (err) => {
+  console.log("Uncaught Exception detected. closeing server...", err);
+  if (server) {
+    server.close(() => {
+      process.exit(1);
+    });
+  }
+  process.exit(1);
+});
+//unhandled rejection 
+process.on("unhandledRejection", (err) => {
+  console.log("Unhandled Rejection detected. closeing server...", err);
+  if (server) {
+    server.close(() => {
+      process.exit(1);
+    });
+  }
+  process.exit(1);
+});
+
+
+
+
+
 main();
 
-//  multer
-// npm i @types/multer
-
-// npm i multer-storage-cloudinary
